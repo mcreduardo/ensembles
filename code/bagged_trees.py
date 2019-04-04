3f4bd62e34f9d7295357c39a7593fc12164640ee
Eduardo Rocha <edu181294@gmail.com>
2019-04-04 16:56:54 -0500

    Implemented bootstrapping for bagging ensemble

---
+++ code/bagged_trees.py
@@ -0,0 +1,64 @@
+# Call: $ ./bagged_trees <#trees> <max depth> <train-set-file> <test-set-file> 
+######################################################################################
+# Eduardo Moura Cirilo Rocha, mouracirilor@wisc.edu
+# April of 2019
+# Bootstrap Aggregation (Bagging)
+######################################################################################
+
+import numpy as np
+import sys
+import json # Load data sets
+import DecisionTree as dt # implementation of DecisionTree
+
+np.random.seed(0) # ensure program is deterministic
+
+printOutput = True
+
+######################################################################################
+# Get inputs and load .json data
+
+# Receive arguments using sys
+numTrees = int(sys.argv[1])
+maxDepth = int(sys.argv[2])
+trainingSetPath = sys.argv[3]
+testSetPath = sys.argv[4]
+
+# Load training and test set
+# metadata + data
+with open(trainingSetPath) as f:
+    trainSet = json.load(f)
+with open(testSetPath) as f:
+    testSet = json.load(f)
+
+# Extract metadata
+features = trainSet["metadata"]["features"]
+
+# Get data
+trainingData = np.array(trainSet["data"])
+testData = np.array(testSet["data"])
+
+trainData_size = len(trainingData)
+
+######################################################################################
+# Generate bootstrap training sets (indices)
+
+bootstrapIndices = np.zeros((trainData_size,numTrees)).astype(int)
+for i in range(numTrees):
+    bootstrapIndices[:,i] = \
+        np.random.choice(trainData_size, size=trainData_size, replace=True)
+
+# print indices of your bootstrapped samples
+
+if printOutput:
+    for row in bootstrapIndices:
+        for j in range(numTrees-1):
+            print(row[j], end= ",")
+        print(row[-1])
+
+######################################################################################
+
+
+
+
+
+
