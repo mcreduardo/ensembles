9501b5d9b5a1367e50376ef14b69250d21662e3f
Eduardo Rocha <edu181294@gmail.com>
2019-04-04 18:01:12 -0500

    Finisshed bagging - implemented testing

--- code/bagged_trees.py
+++ code/bagged_trees.py
@@ -75,7 +75,42 @@ for i in range(numTrees):
     # append to models list
     models.append(mydt)
 
-print(models)
+######################################################################################
+# Testing
+
+# separate features from class
+test_X = testData[:,:-1]
+test_y = testData[:,-1]
+
+individual_prob = [] # init list for computed probabilities
+for tree in models: # predict for all trained models
+    individual_prob.append(tree.predict(test_X, prob=True))
+
+# get average from results
+avg_prob = np.mean(individual_prob, axis = 0)
+
+# print results
+if printOutput:
+    correct_predictions = 0
+    for i in range(len(avg_prob)):
+        for j in range(numTrees):
+            individual_prediction = \
+                features[-1][-1][np.argmax(individual_prob[j][i])]
+            print(individual_prediction, end= ",")
+        bagging_prediction = features[-1][-1][np.argmax(avg_prob[i])]
+        print(bagging_prediction, end= ",")
+        print(test_y[i], end= "\n")
+
+        # correct preditction?
+        if bagging_prediction == test_y[i]: correct_predictions += 1
+    
+    print("\n"+str(correct_predictions/len(avg_prob)))
+
+
+
+    
+
+
 
 
 
