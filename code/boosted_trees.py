c3d144d49c91c022392441c4dfb5e673edd6bf7a
Eduardo Rocha <edu181294@gmail.com>
2019-04-05 15:33:46 -0500

    implemented AdaBoost training

--- code/boosted_trees.py
+++ code/boosted_trees.py
@@ -10,15 +10,13 @@ import sys
 import json # Load data sets
 import DecisionTree as dt # implementation of DecisionTree
 
-np.random.seed(0) # ensure program is deterministic
-
 printOutput = True
 
 ######################################################################################
 # Get inputs and load .json data
 
 # Receive arguments using sys
-numTrees = int(sys.argv[1])
+maxTrees = int(sys.argv[1])
 maxDepth = int(sys.argv[2])
 trainingSetPath = sys.argv[3]
 testSetPath = sys.argv[4]
@@ -38,5 +36,53 @@ trainingData = np.array(trainSet["data"])
 testData = np.array(testSet["data"])
 
 trainData_size = len(trainingData)
+numClasses = len(features[-1][-1])
 
 ######################################################################################
+# Multi-class AdaBoost
+# Following Hastie proposed implementation
+# https://web.stanford.edu/~hastie/Papers/samme.pdf
+
+# init weights
+wi = np.ones(trainData_size)/trainData_size
+
+classifiers = [] # list to store classifiers
+weights = [] # list to store classifiers
+classifiers_scores = [] # list to store scores of classifiers
+for i in range(maxTrees):
+
+    # Fit a classifier T^(m)(X) to the training data using weights wi
+    tree = dt.DecisionTree()
+    train_X = trainingData[:,:-1]
+    train_y = trainingData[:,-1]
+    tree.fit(train_X, train_y, features, max_depth=maxDepth, instance_weights=wi)
+
+    # compute error
+    predictions = tree.predict(train_X, prob=False)
+    error = np.sum(wi * (predictions != train_y)) / np.sum(wi)
+
+    # compute score
+    alpha = np.log( (1-error)/error * (numClasses - 1) )
+
+    # if score is negative: break  (discard this classifier)
+    #if alpha < 0:
+    if error >= 1 - (1/numClasses):
+        break
+    classifiers.append(tree)
+    classifiers_scores.append(alpha)
+    weights.append(wi)
+
+    # update weights
+    wi = wi * np.exp( alpha * (predictions != train_y) )
+    wi = wi/np.sum(wi) # normalize
+
+numClassifiers = len(classifiers)
+
+# print weights
+if printOutput:
+    for i in range(len(wi)):
+        for j in range(numClassifiers - 1):
+            print("%.12f"%weights[j][i], end=",")
+        print("%.12f"%weights[numClassifiers-1][i])
+    print("")
+
